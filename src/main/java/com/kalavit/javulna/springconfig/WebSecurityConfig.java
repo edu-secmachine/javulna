5f1ee12604aa8ccfbd22cd050228da71697f509f
Nyilasy Péter <nyilasypeter@gmail.com>
2019-03-14 12:46:21 +0100

    on unauthorized requests send http 403 instead of redirect

--- src/main/java/com/kalavit/javulna/springconfig/WebSecurityConfig.java
+++ src/main/java/com/kalavit/javulna/springconfig/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.kalavit.javulna.springconfig;
 
+import java.io.IOException;
 import java.util.Arrays;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.BeanIds;
@@ -11,28 +16,31 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+    
     @Autowired
     UserDetailsService userDetailsService;
-
+    
     @Autowired
     CustomAuthenticationSuccessHandler successHandler;
-
+    
     @Autowired
     CustomAuthenticationFailureHandler failureHandler;
-
+    
     @Autowired
     CustomLogoutSuccessHandler logouthandler;
-
+    
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
@@ -44,16 +52,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration(MyServletConfig.CORS_MAPPNIG, configuration);
         return source;
     }
-
+    
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
         UserDetailsService manager = new UserDetailsServiceImpl();
         return manager;
     }
-
+    
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
     
@@ -63,14 +71,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
-
+        
     }
-
+    
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider());
     }
-
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -80,6 +88,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("username")
                 .successHandler(successHandler)
                 .failureHandler(failureHandler)
+                .and()
+                .exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+                    }
+                })
                 .and().logout().logoutSuccessHandler(logouthandler)
                 .and().cors()
                 .and().authorizeRequests()
@@ -87,11 +102,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/rest/user/**").permitAll()
                 .anyRequest().authenticated();
     }
-
+    
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
+    
 }
