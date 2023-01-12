aa068bcb22708f64182b9804cd1a81db2cfe2653
Heller <thomas.heller@cgi.com>
2023-01-11 20:03:35 -0500

    doin'it right...

--- src/main/java/com/kalavit/javulna/services/FuzzerService.java
+++ src/main/java/com/kalavit/javulna/services/FuzzerService.java
@@ -1,3 +1,5 @@
+package com.kalavit.javulna.services;
+
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
 public class ExampleFuzzer {
@@ -5,12 +7,5 @@ public class ExampleFuzzer {
         int number = data.consumeInt();
         String string = data.consumeRemainingAsString();
         
-        // The function for fuzzing the changePassword function (just a test: should try on more functions and longer code bases...)
-        public static void fuzzerTestOneInput(FuzzedDataProvider data) {
-
-            int number = data.consumeInt();
-            String string = data.consumeRemainingAsString();
-
-            changePassword(string);
-    }
+        changePassword(string);
 }
